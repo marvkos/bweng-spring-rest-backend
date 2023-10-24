ec92f5cff08207ccdcc570078153af619b1628fb
michi-1 <reiter.michael@protonmail.com>
2023-10-24 20:43:51 +0200

    Validation added

--- src/main/java/at/technikum/springrestbackend/model/Phone.java
+++ src/main/java/at/technikum/springrestbackend/model/Phone.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,19 +25,19 @@ public class Phone {
     @Setter(AccessLevel.NONE)
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-
+    @NotBlank
     private String name;
 
     private String description;
-
+    @Positive
     private float displaySize;
-
+    @Min(0)
     private int memory;
-
+    @Min(0)
     private int battery;
-
+    @Positive
     private float price;
-
+    @NotBlank
     private String brand;
 
     public Phone(String name, String description, float displaySize, int memory, int battery, float price, String brand){
