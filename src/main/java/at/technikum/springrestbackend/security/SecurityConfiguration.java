718058dcc2d00842e8a8057b2403bf0cf7e0e050
michi-1 <104499339+michi-1@users.noreply.github.com>
2023-12-05 12:12:47 +0100

    Add files via upload

---
+++ src/main/java/at/technikum/springrestbackend/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package at.fhtw.be_webeng_23ws.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        httpSecurity.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+    }
+}
