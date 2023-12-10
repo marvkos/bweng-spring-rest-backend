e947b2a56a4fc9858db7d07fe33082b5c09db601
Nico Jöch <wi21b032@technikum-wien.at>
2023-12-10 16:33:10 +0100

    added CORS specification & changed TokenResponse model

--- src/main/java/at/technikum/springrestbackend/dto/TokenResponse.java
+++ src/main/java/at/technikum/springrestbackend/dto/TokenResponse.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class TokenResponse {
     private String token;
+    private UUID id;
 }
