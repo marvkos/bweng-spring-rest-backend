31f6103112f7ee6ce47c42abe6f39acc987b399c
VanessaMaria02 <wi21b004@technikum-wien.at>
2024-01-06 21:51:18 +0100

    fix User update funktion

--- src/main/java/at/technikum/springrestbackend/repository/UserRepository.java
+++ src/main/java/at/technikum/springrestbackend/repository/UserRepository.java
@@ -34,7 +34,7 @@ public interface UserRepository extends CrudRepository<User, UUID> {
     @Override
     List<User> findAll();
     @Modifying
-    @Query("UPDATE User u SET u.username = :newUsername,u.password = :newPassword, u.role = :newRole, u.firstname = :newFirstname, u.lastname = :newLastname, u.salutation = :newSalutation, u.email = :newEmail, u.countryCode = :newCountryCode, u.postalCode = :newPostalCode, u.street = :newStreet, u.city = :newCity, u.houseNumber = :newHouseNumber, u.profilePicture = :newProfilePicture, u.salutation = :newStatus WHERE u.username = :oldUsername")
+    @Query("UPDATE User u SET u.username = :newUsername,u.password = :newPassword, u.role = :newRole, u.firstname = :newFirstname, u.lastname = :newLastname, u.salutation = :newSalutation, u.email = :newEmail, u.countryCode = :newCountryCode, u.postalCode = :newPostalCode, u.street = :newStreet, u.city = :newCity, u.houseNumber = :newHouseNumber, u.profilePicture = :newProfilePicture, u.salutation = :newSalutation, u.status = :newStatus WHERE u.username = :oldUsername")
     int updateUserInfo(@Param("oldUsername") String oldUsername, @Param("newUsername") String newUsername,@Param("newPassword") String newPassword,@Param("newRole") String newRole,@Param("newFirstname") String newFirstname, @Param("newLastname") String newLastname, @Param("newSalutation") String newSalutation,
     @Param("newEmail") String newEmail, @Param("newCountryCode") String newCountryCode, @Param("newPostalCode") int newPostalCode, @Param("newStreet") String newStreet, @Param("newCity") String newCity, @Param("newHouseNumber") String newHouseNumber,  @Param("newProfilePicture") String newProfilePicture, @Param("newStatus") boolean newStatus);
 }
