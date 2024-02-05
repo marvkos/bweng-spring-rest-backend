52f23359967d4719ade8dafcd60931a970688559
Neliax <xialin.ye1996@hotmail.com>
2024-02-05 14:56:53 +0100

    test push

--- src/main/java/at/technikum/springrestbackend/controller/UserController.java
+++ src/main/java/at/technikum/springrestbackend/controller/UserController.java
@@ -1,5 +1,77 @@
 package at.technikum.springrestbackend.controller;
 
+import at.technikum.springrestbackend.dto.UserDTO;
+import at.technikum.springrestbackend.mapper.UserMapper;
+import at.technikum.springrestbackend.model.UserModel;
+import at.technikum.springrestbackend.repository.UserRepository;
+import at.technikum.springrestbackend.services.UserServices;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin
 public class UserController {
+    private final UserMapper userMapper;
+    private final UserServices userServices;
+    private final UserRepository userRepository;
+    @Autowired
+    public UserController(UserMapper userMapper,
+                          UserServices userServices,
+                          UserRepository userRepository) {
+        this.userMapper = userMapper;
+        this.userServices = userServices;
+        this.userRepository = userRepository;
+    }
+    @GetMapping
+    public List<UserDTO> readAll() {
+        return userServices.findAll().stream()
+                .map(userMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+    @GetMapping("/{id}")
+    public UserDTO read(@PathVariable String id) {
+        UserModel user = userServices.find(id);
+        return userMapper.toDTO(user);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO create(@RequestBody @Valid UserDTO userDTO){
+        UserModel user = userMapper.toEntity(userDTO);
+        userServices.save(user);
+        return userMapper.toDTO(user);
+    }
+
+    @PutMapping("/{id}")
+    public UserDTO update(@PathVariable String id, @RequestBody UserDTO updatedUserDTO){
+
+        return userMapper.toDTO(userServices.update(id, updatedUserDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public UserDTO delete(@PathVariable String id){
+        UserDTO deletedUserDTO =
+                new UserDTO(
+                        id, userServices.find(id).getUsername(),
+                        userServices.find(id).getPw(),
+                        userServices.find(id).getCountry(),
+                        userServices.find(id).getAddress(),
+                        userServices.find(id).getFirstname(),
+                        userServices.find(id).getSurname(),
+                        userServices.find(id).getEmail(),
+                        userServices.find(id).getGender()
+                );
+
+        userRepository.deleteById(id);
+        return deletedUserDTO;
+    }
 
 }
+
+
--- src/main/java/at/technikum/springrestbackend/model/UserModel.java
+++ src/main/java/at/technikum/springrestbackend/model/UserModel.java
@@ -1,4 +1,124 @@
 package at.technikum.springrestbackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import org.apache.catalina.User;
+import org.springframework.core.annotation.Order;
+
+@Entity
+@Table(name = "users")
 public class UserModel {
+
+    @Id
+    private String id;
+    private String username;
+    private String pw;
+    private String gender;
+    private String firstname;
+    private String surname;
+    private String email;
+    private String country;
+    private String address;
+
+
+    protected UserModel() {}
+
+    public UserModel(String id, String username, String pw, String country, String address, String firstname, String surname, String email, String gender) {
+        this.id = id;
+        this.username = username;
+        this.pw = pw;
+        this.country = country;
+        this.address = address;
+        this.firstname = firstname;
+        this.surname = surname;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public void setAllEntity(String id, String username, String pw, String country, String address, String firstname, String surname, String email, String gender) {
+        setCountry(country);
+        setUsername(username);
+        setAddress(address);
+        setEmail(email);
+        setFirstname(firstname);
+        setSurname(surname);
+        setGender(gender);
+        setPw(pw);
+        setId(id);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
