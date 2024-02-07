31636f66b6bf47a45b0d8d887bf7df36be5f02b2
Neliax <xialin.ye1996@hotmail.com>
2024-02-07 12:57:35 +0100

    User Controllers

--- src/main/java/at/technikum/springrestbackend/mapper/UserMapper.java
+++ src/main/java/at/technikum/springrestbackend/mapper/UserMapper.java
@@ -10,55 +10,41 @@ import java.util.UUID;
 public class UserMapper {
 
     public UserDTO toDTO(UserModel userModel) {
-        //creating a new DTO of User
+        //creating a new DTO of User to assign the values of the Entity to it
         UserDTO newUserDTO = new UserDTO();
         //assigning ID, PW and USERNAME to the DTO by using SETTER
-        newUserDTO.setAllDTO(userModel.getId(),userModel.getUsername(),
+        newUserDTO.setAllDTO(userModel.getId(), userModel.getUsername(),
                              userModel.getPw(), userModel.getCountry(),
-                             userModel.getAddress(), userModel.getFirstname(),
-                             userModel.getSurname(), userModel.getEmail(),
-                             userModel.getGender()
-                            );
-
+                             userModel.getAddress(), userModel.getName(),
+                             userModel.getEmail());
         return newUserDTO;
     }
 
     public UserModel toEntity(UserDTO userDTO) {
-
         //TODO: security with PassWord needed (Hashing?)
+        //DataBank entry requires the id as a primary key
         if (userDTO.getId() == null) {
-            //UserModel does not have an empty Constructor
-            //--> Object creation requires ID, PW, USERNAME
             return new UserModel(
                         UUID.randomUUID().toString(),
                         userDTO.getUsername(), userDTO.getPw(),
                         userDTO.getCountry(), userDTO.getAddress(),
-                        userDTO.getFirstname(), userDTO.getSurname(),
-                        userDTO.getEmail(), userDTO.getGender()
-            );
+                        userDTO.getName(),
+                        userDTO.getEmail());
         }
-
 //      ALTERNATIVELY:
 //        if (userDTO.getId() == null) {
-//
 //            UserModel newUserModel = new UserModel(
 //                          UUID.randomUUID().toString(),
-//                          userDTO.getUsername(),
-//                          userDTO.getPw(),
 //                          ...
 //                      );
-//
 //            return newUserModel;
 //        }
-
-
         return new UserModel(
                     userDTO.getId(),
                     userDTO.getUsername(), userDTO.getPw(),
                     userDTO.getCountry(), userDTO.getAddress(),
-                    userDTO.getFirstname(), userDTO.getSurname(),
-                    userDTO.getEmail(), userDTO.getGender()
-        );
+                    userDTO.getName(),
+                    userDTO.getEmail());
     }
 
 
