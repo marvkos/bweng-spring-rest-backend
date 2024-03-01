8e5f01f7d6db4bd08836c870887e07c2ee31e3eb
Neliax <xialin.ye1996@hotmail.com>
2024-03-01 13:54:56 +0100

    First Working Version

--- src/main/java/at/technikum/springrestbackend/repository/ChatRoomRepository.java
+++ src/main/java/at/technikum/springrestbackend/repository/ChatRoomRepository.java
@@ -1,6 +1,5 @@
 package at.technikum.springrestbackend.repository;
 
-import at.technikum.springrestbackend.model.ChatMessageModel;
 import at.technikum.springrestbackend.model.ChatRoomModel;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
