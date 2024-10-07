50b5267164c37e1894628e362035efb0859026d7
Neliax <xialin.ye1996@hotmail.com>
2024-10-07 16:57:31 +0200

    test push no changes

--- src/main/java/at/technikum/springrestbackend/security/JwtUtil.java
+++ src/main/java/at/technikum/springrestbackend/security/JwtUtil.java
@@ -20,6 +20,7 @@ public class JwtUtil {
     @Value("${jwt.expiration}")
     private long jwtExpirationInMs;
 
+
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
